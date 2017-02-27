2767388b721e4d7e407199be1565ddb676a693f9
Fryslie <brandon@fryslie.com>
2017-02-27 15:38:18 -0700

    add script to fix dotfile symlinks.  move dotfiles into 'dotfiles' directory

--- fix_dotfiles.sh
+++ fix_dotfiles.sh
@@ -1,11 +1,25 @@
-files="gemrc rvmrc zgen-setup.zsh zshrc tmux.shared.conf"    # list of files/folders to symlink in homedir
+#/usr/bin/zsh
+dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/dotfiles"
 
-echo "Reseting symlinks in $HOME to point to $HOME/dotfiles"
+files=`cd $dotfiles_dir && ls`    # list of files/folders to symlink in homedir
+
+
+echo "Reseting symlinks in $HOME to point to $dotfiles_dir/*"
 for file in $files; do
-    file_path="$HOME/.$file"
-    rm $file_path 2>/dev/null
-    echo "Creating symlink $HOME/dotfiles/$file -> $file_path"
-    ln -s "$HOME/dotfiles/$file" "$file_path"
+    dotfile_path="$dotfiles_dir/$file"
+    symlink_path="$HOME/.$file"
+    if [[ ! -h $symlink_path ]]; then
+        backup_dir=$HOME/.dotfile_backup
+        mkdir -p $backup_dir
+        echo "Backing up existing non-symlinked dotfile to ~/.dotfile_backup"
+        mv $symlink_path $backup_dir
+    else
+        echo "Removing existing symlink at '$symlink_path'"
+        rm -f $symlink_path
+    fi
+    echo "Creating symlink $dotfile_path -> $symlink_path"
+    ln -s "$dotfile_path" "$symlink_path"
+    echo
 done
 
 echo "Done!"
