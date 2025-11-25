11e7003e911c0376c67aefcb836b6579ecf59e81
Brandon Fryslie <nunya@hotmail.com>
2025-11-25 10:22:24 -0700

    Add neovim config

---
+++ config/nvim/lua/plugins/n00b-keys.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    -- any string is fine here if you're using `dir`
+    "loom99/n00bkeys",
+    dir = "~/code/brandon-fryslie_neovim-plugin-n00bkeys",
+    name = "n00bkeys",
+    lazy = false,
+  },
+}
