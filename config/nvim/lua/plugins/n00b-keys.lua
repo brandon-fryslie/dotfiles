4c5d01cee32883a02d68838b8efcd568e05733ea
Brandon Fryslie <nunya@hotmail.com>
2025-12-09 05:35:22 -0700

    Update n00bkeys plugin to use GitHub repo

--- config/nvim/lua/plugins/n00b-keys.lua
+++ config/nvim/lua/plugins/n00b-keys.lua
@@ -1,9 +1,6 @@
 return {
   {
-    -- any string is fine here if you're using `dir`
-    "loom99/n00bkeys",
-    dir = "~/code/brandon-fryslie_neovim-plugin-n00bkeys",
-    name = "n00bkeys",
+    "loom99-public/n00bkeys.nvim",
     lazy = false,
   },
 }
